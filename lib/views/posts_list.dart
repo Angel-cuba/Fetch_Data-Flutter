b586261310a9788eb903f777545ce871e7d2d238
Mac_Developer <araozangel842@gmail.com>
2023-02-02 22:14:15 +0200

    Fetch data from network api

--- lib/views/posts_list.dart
+++ lib/views/posts_list.dart
@@ -1,4 +1,7 @@
+import 'package:fetch_data_api/api/request.dart';
 import 'package:flutter/material.dart';
+
+import 'post_details.dart';
 
 class ListOfPosts extends StatefulWidget {
   const ListOfPosts({super.key});
@@ -8,13 +11,57 @@ class ListOfPosts extends StatefulWidget {
 }
 
 class _ListOfPostsState extends State<ListOfPosts> {
+  final Future<List<Map>> _listPosts = HTTPRequest().getItems();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Posts'),
       ),
-      body: Container(),
+      body: FutureBuilder<List<Map>>(
+          future: _listPosts,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error fetching posts ${snapshot.error}'),
+              );
+            }
+            if (snapshot.hasData) {
+              List<Map> data = snapshot.data;
+              return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Builder(builder: (context) {
+                    return Card(
+                      elevation: 5.0,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0, vertical: 6.0),
+                      color: Colors.white70,
+                      surfaceTintColor: Colors.yellowAccent,
+                      child: ListTile(
+                        title: Text(data[index]['title']),
+                        subtitle: Text(data[index]['body']),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PostDetails(
+                                data[index]['id'],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  });
+                },
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
     );
   }
 }
